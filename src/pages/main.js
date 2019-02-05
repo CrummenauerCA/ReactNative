f1f71a8aa2a92c89c786974d1717621131f582fa
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-04 22:30:35 -0200

    Adicionando uma chave em itens da listagem...

--- src/pages/main.js
+++ src/pages/main.js
@@ -19,7 +19,7 @@ export default class Main extends Component {
         const response = await api.get('/products');
         const { docs } = response.data;
         console.log(docs);
-        this.setState ({
+        this.setState({
             docs
         });
         console.log(docs);
@@ -29,7 +29,7 @@ export default class Main extends Component {
         return (
             <View>
                 <Text>Número de itens: {this.state.docs.length}</Text>
-                {this.state.docs.map(product => <Text>{product.title}</Text>)}
+                {this.state.docs.map(product => <Text key={product._id}>{product.title}</Text>)}
             </View>
         );
     }
