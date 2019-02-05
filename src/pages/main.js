4452143780c381cd5483fae6768d094036274f5b
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-04 23:25:59 -0200

    Estilizando os elementos...

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from "react-native";
 import api from '../services/api';
 
 export default class Main extends Component {
@@ -18,28 +18,77 @@ export default class Main extends Component {
     loadProducts = async () => {
         const response = await api.get('/products');
         const { docs } = response.data;
-        console.log(docs);
         this.setState({
             docs
         });
-        console.log(docs);
+        // console.log(docs);
     };
 
     renderItem = ({ item }) => (
-        <View>
-            <Text>{item.title}</Text>
-            <Text>{item.description}</Text>
-            <TouchableOpacity onPress={() => {}}>
-                <Text>Acessar</Text>
+        <View style={styles.productContainer}>
+            <Text style={styles.productTitle}>{item.title}</Text>
+            <Text style={styles.productDescription}>{item.description}</Text>
+            <TouchableOpacity style={styles.productButton} onPress={() => { }}>
+                <Text style={styles.productButtonText}>Acessar</Text>
             </TouchableOpacity>
         </View>
     );
 
+
     render() {
         return (
-            <View>
-                <FlatList data={this.state.docs} keyExtractor={item => item._id} renderItem={this.renderItem} />
+            <View style={styles.container}>
+                <FlatList
+                    contentContainerStyle={styles.list}
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+                />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#e5e5e5'
+    },
+    list: {
+        padding: 20
+    },
+    productContainer: {
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 20,
+        padding: 20,
+        marginBottom: 20
+    },
+    productTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333'
+    },
+    productDescription: {
+        fontSize: 16,
+        color: '#999',
+        marginTop: 5,
+        lineHeight: 18
+    },
+    productButton: {
+        height: 42,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#da552f',
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10
+    },
+    productButtonText: {
+        fontSize: 16,
+        color: '#da552f',
+        fontWeight: 'bold'
+    }
+});
